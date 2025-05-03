411b088ed2f651aa35fd5d0e241f29068b7c1a08
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-05-03 21:30:15 +0530

    CodeChef

---
+++ july/july222/concatsort.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    sorted_a = sorted(a)
+    i = 0
+    l = 0
+    for idx in range(n):
+        if sorted_a[i] == a[idx]:
+            i += 1 
+        else:
+            if a[idx] < l:
+                print("NO")
+                break
+            l = a[idx]
+    else:
+        print("YES")
