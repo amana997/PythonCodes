13cb5473d3d012e0deb4c0dc44c652a9e112d944
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-05-03 21:59:05 +0530

    CodeChef

---
+++ ltime/ltime83/ffl.py
@@ -0,0 +1,19 @@
+for _ in range(int(input())):
+    n, s = map(int, input().split())
+    p = list(map(int, input().split()))
+    a = list(map(int, input().split()))
+    budget = 100 - s
+    defender, forward = [], []
+    for i in range(n):
+        if a[i] == 0:
+            defender.append(p[i])
+        elif a[i] == 1:
+            forward.append(p[i])
+    
+    if len(defender) == 0 or len(forward) == 0:
+        print("no")
+    else:
+        if min(forward) + min(defender) <= budget:
+            print("yes")
+        else:
+            print("no")
