771621860d79b62e584234c9e7092fe4da8c8964
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-05-13 16:10:33 +0530

    CodeChef

---
+++ start/start1/divisor.py
@@ -0,0 +1,22 @@
+MAXN = 10**6
+
+divisors = [0] * (MAXN + 1)
+for i in range(1, MAXN + 1):
+    for j in range(i, MAXN + 1, i):
+        divisors[j] += 1
+
+valid_divisors = set()
+for i in range(1, MAXN + 1):
+    if divisors[i] == 4:
+        valid_divisors.add(i)
+
+min_divisors = divisors[:]
+
+for i in range(1, MAXN + 1):
+    for j in range(i, MAXN + 1, i):
+        if i in valid_divisors:
+            min_divisors[j] = min(min_divisors[j], min_divisors[j // i])
+
+for _ in range(int(input())):
+    n = int(input())
+    print(min_divisors[n])
