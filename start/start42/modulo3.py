ff7e4e4edf664c4172ca6959221b5b1aa5f69f7b
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-05-17 11:37:03 +0530

    CodeChef

---
+++ start/start42/modulo3.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    
+    if a % 3 == 0 or b % 3 == 0:
+        print(0)
+    elif a % 3 == b % 3:
+        print(1)
+    else:
+        print(2)
