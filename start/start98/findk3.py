d8aab4d310f62a0d239a22887c4a8e3199291b2d
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-05-25 10:11:27 +0530

    CodeChef

---
+++ start/start98/findk3.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    x, y, z = map(int,input().split())
+    
+    if (x * y) % z == 0:
+        print(x * y, z)
+    elif (y * z) % x == 0:
+        print(y * z, x)
+    elif (x * z) % y == 0:
+        print(x * z, y)
+    else:
+        print(-1)
